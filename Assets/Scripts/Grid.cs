3fbaff2fa6224f3a41902f17eedb3a5bedced968
Henno Théo <94365544+NazioLT@users.noreply.github.com>
2024-03-25 21:39:24 +0100

    place plants

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,9 +1,30 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Script qui place et retire les plantes de la grille.
+/// </summary>
 public class Grid : MonoBehaviour
 {
+    [SerializeField] private Plant _plant = null;
+
+    public static Vector3 GridToWorldSpace(Vector2 square)
+    {
+        return new Vector3(
+                square.x + 0.5f,
+                0f,
+                square.y + 0.5f
+            );
+    }
+
     public void Select(Vector2Int position)
     {
         Debug.Log("position " + position);
+        PlacePlant(_plant, position);
+    }
+
+    private void PlacePlant(Plant plantPrefab, Vector2Int position)
+    {
+        Plant plant = Instantiate(plantPrefab);
+        plant.Place(position);
     }
 }
---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+/// <summary>
+/// Script général de la plante, gère les intéractions principales de la plante.
+/// </summary>
+public class Plant : MonoBehaviour
+{
+    private Vector2Int _square;
+
+    public void Place(Vector2Int square)
+    {
+        _square = square;
+        transform.position = Grid.GridToWorldSpace(square);
+    }
+}
