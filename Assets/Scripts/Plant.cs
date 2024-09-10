5d2e51b90042f2b3498f203c2e97f45fcfe8209d
Henno Théo <94365544+NazioLT@users.noreply.github.com>
2024-09-10 15:58:59 +0200

    ep-7

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -4,6 +4,14 @@
 /// </summary>
 public class Plant : Character
 {
+    [SerializeField] private float _attackCooldown = 0.5f;
+    [SerializeField] private float _attackDistance = 6f;
+    [SerializeField] private Projectile _projectile = null;
+    [SerializeField] private LayerMask _zombieMask;
+
+    private float _currentAttackCooldown = 0f;
+    private bool _canAttack = false;
+
     private Vector2Int _position;
 
     public void Place(Vector2Int position)
@@ -15,5 +23,29 @@ public class Plant : Character
     public void Kill()
     {
         Destroy(gameObject);
+    }
+
+    private void FixedUpdate()
+    {
+        _canAttack = Physics.Raycast(transform.position + Vector3.up, Vector3.right, _attackDistance, _zombieMask);
+    }
+
+    private void Update()
+    {
+        _currentAttackCooldown -= Time.deltaTime;
+
+        if (_currentAttackCooldown < 0 && _canAttack)
+            Attack();
+    }
+
+    private void Attack()
+    {
+        if (_currentAttackCooldown > 0f)
+            return;
+
+        _currentAttackCooldown = _attackCooldown;
+
+        Projectile proj = Instantiate(_projectile, transform.position + Vector3.up, Quaternion.identity);
+        proj.Launch(Vector3.right);
     }
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10;
+    [SerializeField] private float _radius = 1f;
+    [SerializeField] private int _damages = 1;
+    [SerializeField] private LayerMask _mask;
+
+    private Rigidbody _rigidbody;
+
+    public void Launch(Vector3 direction)
+    {
+        _rigidbody.velocity = direction * _speed;
+    }
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _radius, _mask);
+
+        foreach (Collider col in colliders)
+        {
+            if (!col.TryGetComponent(out Life life))
+                continue;
+
+            life.Damage(_damages);
+            Destroy(gameObject);
+            return;
+        }
+    }
+}
